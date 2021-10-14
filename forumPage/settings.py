6914e055ef7c9253030b10567cceda31954dcb92
Vahan Dag <vahandag@gmail.com>
2021-10-14 17:48:28 +0300

    static change

--- forumPage/settings.py
+++ forumPage/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 # STATIC_URL = '/static/'
