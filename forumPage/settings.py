c38be9c1210f5406cc1a5cf4369c8d72985e0bd9
Vahan Dag <vahandag@gmail.com>
2021-10-14 20:07:31 +0300

    allowed

--- forumPage/settings.py
+++ forumPage/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = False
 
 # ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-ALLOWED_HOSTS = ["desponia-page.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["desponia-page.herokuapp.com"]
 
 
 # Application definition
