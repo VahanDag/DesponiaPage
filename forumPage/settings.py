7cffc4273e1cf100b7238b680c51df79f99ff9d4
Vahan Dag <80410212+VahanDag@users.noreply.github.com>
2021-11-01 23:16:09 +0300

    Update settings.py

--- forumPage/settings.py
+++ forumPage/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = True
 
 # ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-ALLOWED_HOSTS = ["vahandag.pythonanywhere.com"]
+ALLOWED_HOSTS = ["www.desponia.com","despona.com"]
 
 
 # Application definition
